a952f0810a508bff7a7da9bd1eed02fb684e47b3
George Hannah <georgehannah@hotmail.co.uk>
2024-06-08 06:19:07 +0100

    added typescript setup and gdscript setup

--- init.lua
+++ init.lua
@@ -582,6 +582,7 @@ require('lazy').setup({
         pylsp = {},
         rust_analyzer = {},
         jdtls = {},
+        tsserver = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
